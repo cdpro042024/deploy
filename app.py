8b38505f9d7277745f1975e2aef06c7c76c7befe
Eduardo <167145745+cdpro042024@users.noreply.github.com>
2024-04-15 20:51:07 -0300

    Atualizei o app.py
    Coloquei o teste no codigo

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
+# Teste
 # Gerando dados fictícios
 def generate_data():
     date_range = pd.date_range(start='2020-01-01', end='2024-12-31')
